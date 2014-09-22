3315e7dbf99afc2dedd6caaf2e22bb6f354ab3ce
万洲 <wanzhou.njust2010@gmail.com>
2014-09-22 15:05:41 +0800

    update helloworld.java

--- src/helloworld.java
+++ src/helloworld.java
@@ -1,6 +1,7 @@
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Update helloworld.java");
     }
 }
 
